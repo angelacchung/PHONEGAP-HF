8951976ba8ea384022b54169807610384c8f8ca0
angelacchung <44277537+angelacchung@users.noreply.github.com>
2019-04-21 23:21:52 -0400

    dsds

--- js/main.js
+++ js/main.js
@@ -4,4 +4,40 @@ $("#startBtn").click(function()
         $("#startBtn").hide();
         $("#infoBtn").hide();
         $(".hiddenFriend").hide();
+        $("#firstResp").show();
+        $("#dialogue").show();
+
     })
+
+$("#dia1").click(function(){
+    console.log("dialogue 2");
+    $("#firstResp").hide();
+    $("#dialogue").hide();
+    $("#secondResp").show();
+    $("#happyResp").show();
+})
+
+$("#dia2").click(function(){
+    console.log("dia2");
+    $("#firstResp").hide();
+    $("#dialogue").hide();
+    $("#thirdResp").show();
+    $("#dialogue2").show();
+})
+
+$("#dia3").click(function(){
+    console.log("dia3");
+    $("#thirdResp").hide();
+    $("#dialogue2").hide();
+
+    $("#fourthResp").show();
+
+})
+$("#dia4").click(function(){
+    console.log("dia3");
+    $("#thirdResp").hide();
+    $("#dialogue2").hide();
+
+    $("#fifthResp").show();
+
+})
